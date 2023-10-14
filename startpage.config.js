f8ec951d4c9409fd437393d0fdabd166f3bdd7f0
Zabooby <78969986+Zabooby@users.noreply.github.com>
2023-10-14 08:34:14 +0100

    Update startpage.config.js

--- startpage.config.js
+++ startpage.config.js
@@ -258,7 +258,7 @@ const defaultConfig = {
 					},
 					{
 						name: "Loading Artist",
-						url: "https://loadingartist.com/comic/user-terror",
+						url: "https://loadingartist.com/comic/plucked-up-helping-hand",
 						icon: "mdi:comic-bubble"
 					},
 					{
