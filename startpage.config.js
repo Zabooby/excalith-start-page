d36317d7ef9a4a75205e287f1bb7fc62756c6ecf
Zabooby <78969986+Zabooby@users.noreply.github.com>
2024-02-18 08:25:46 +0000

    Update startpage.config.js

--- startpage.config.js
+++ startpage.config.js
@@ -258,7 +258,7 @@ const defaultConfig = {
 					},
 					{
 						name: "Loading Artist",
-						url: "https://loadingartist.com/comic/rose-to-the-occasions",
+						url: "https://loadingartist.com/comic/rose-to-the-occasion",
 						icon: "mdi:comic-bubble"
 					},
 					{
