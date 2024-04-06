ddfc924818e07c0b95156241423384807a32c31c
Zabooby <78969986+Zabooby@users.noreply.github.com>
2024-04-06 08:53:01 +0100

    Update startpage.config.js

--- startpage.config.js
+++ startpage.config.js
@@ -258,7 +258,7 @@ const defaultConfig = {
 					},
 					{
 						name: "Loading Artist",
-						url: "https://loadingartist.com/comic/sketchy-neighbourhood",
+						url: "https://loadingartist.com/comic/sticky-situation",
 						icon: "mdi:comic-bubble"
 					},
 					{
