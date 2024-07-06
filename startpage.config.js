5d6e882c2e3634886305f5d1bfe763e5ae805128
Zabooby <78969986+Zabooby@users.noreply.github.com>
2024-07-06 08:11:26 +0100

    Update startpage.config.js

--- startpage.config.js
+++ startpage.config.js
@@ -258,7 +258,7 @@ const defaultConfig = {
 					},
 					{
 						name: "Loading Artist",
-						url: "https://loadingartist.com/comic/hungry-for-home",
+						url: "https://loadingartist.com/comic/moved",
 						icon: "mdi:comic-bubble"
 					},
 					{
