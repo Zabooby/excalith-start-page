0cd2b5f666ee5fa5d9603ad2987e4053da7d8a76
Zabooby <78969986+Zabooby@users.noreply.github.com>
2024-08-03 10:10:50 +0100

    Update startpage.config.js

--- startpage.config.js
+++ startpage.config.js
@@ -258,7 +258,7 @@ const defaultConfig = {
 					},
 					{
 						name: "Loading Artist",
-						url: "https://loadingartist.com/comic/comic-relief",
+						url: "https://loadingartist.com/comic/final-offer",
 						icon: "mdi:comic-bubble"
 					},
 					{
