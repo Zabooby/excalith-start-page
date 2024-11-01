236bfd521d247d66cb63fa4a5297f97eb8bc3927
Zabooby <78969986+Zabooby@users.noreply.github.com>
2024-11-01 08:29:38 +0000

    Update startpage.config.js

--- startpage.config.js
+++ startpage.config.js
@@ -258,7 +258,7 @@ const defaultConfig = {
 					},
 					{
 						name: "Loading Artist",
-						url: "https://loadingartist.com/comic/sign-of-the-times",
+						url: "https://loadingartist.com/comic/candy-man",
 						icon: "mdi:comic-bubble"
 					},
 					{
